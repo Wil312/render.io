c45621085dd592715f310f2d903ad4be4f32245b
Will Yasvoin <wil312@gmail.com>
2026-04-18 15:21:09 +1000

    fix: remove next-themes ThemeProvider to eliminate script-tag warning
    next-themes injects a blocking <script> for theme detection which React
    warns about. render.io is light-only so ThemeProvider is unnecessary.
    - Removed ThemeProvider from Providers.tsx
    - Hardcoded theme='light' in sonner.tsx instead of reading from useTheme()

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
-      <TooltipProvider delayDuration={400}>
-        {children}
-        <Toaster position="bottom-right" />
-      </TooltipProvider>
-    </ThemeProvider>
+    <TooltipProvider delayDuration={400}>
+      {children}
+      <Toaster position="bottom-right" />
+    </TooltipProvider>
   );
 }
